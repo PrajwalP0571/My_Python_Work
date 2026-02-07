4f94ae7946e985c45fb9ae5bd96ec49f182f036d
PrajwalP0571 <prajwal.pawar0571@gmail.com>
2026-02-07 09:43:03 +0530

    My python work is this.

---
+++ Python_Programs_Easy/pythonOperators.py
@@ -0,0 +1,34 @@
+import sys
+
+# Arithmetic Operators
+
+a = 10
+b = 5       
+
+# print(a + b)  # Addition
+# print(a - b)  # Subtraction
+# print(a * b)  # Multiplication
+# print(a / b)  # Division
+# print(a % b)  # Modulus
+# print(a ** b) # Exponentiation
+# print(a // b) # Floor Division  
+
+# Assignment Operators
+
+c = 20
+c += 5  # c = c + 5
+print("After c += 5:", c)
+c -= 5  # c = c - 5
+print("After c -= 5:", c)
+c *= 5  # c = c * 5
+print("After c *= 5:", c)
+c /= 5  # c = c / 5
+print("After c /= 5:", c)
+c %= 5  # c = c % 5
+print("After c %= 5:", c)
+c **= 5 # c = c ** 5
+print("After c **= 5:", c)
+c //= 5 # c = c // 5
+print("After c //= 5:", c)
+
+
